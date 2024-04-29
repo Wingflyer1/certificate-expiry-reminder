fd59a06ffde5478ef92417a35c0d367b6a5770ba
Sten Terje Hovden-Falnes <sten_terje@hotmail.com>
2024-04-29 16:31:48 +0200

    project and app for reminder created, impl.txt created

--- reminderapp/urls.py
+++ reminderapp/urls.py
@@ -14,16 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-
-    # Reminder project urls
-    # path("admin/", admin.site.urls),
-    path("projectadmin/", admin.site.urls),
-
-
-    # Reminder app urls
-    path("reminder/", include("reminderapp.urls")),
+    path("", views.index, name = "index"),
 ]
+
--- reminderapp/views.py
+++ reminderapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, welcome to the certificate-reminder-app.<br><br>Here we'll develop a cool new and usefull app for people with,<br>a need to keep their papers up to date.")
